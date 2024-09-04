3da364d56679b275f257827111b7a9ebf5320730
leathens <leathens@ut.utm.edu>
2024-09-04 16:54:00 -0500

    updated main to get sum of user's number

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+
+int sum(int n) {
+  int total = 0;
+  for (int i=1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
 int main() {
 
-  cout << "Hello World!" << endl;
+  int n;
+
+  cout << "Enter a number. Please?" << endl;
+  cin >> n;
+
+  cout << "Your sum is " << sum(n) << "! :)" << endl;
   
   return 0;
 }
